8624f0241721a48ae164077008cfda7efe62d325
Jonik <zenaserdykov2004@gmail.com>
2022-10-18 23:28:51 +0300

    masterBranch

---
+++ LoginForm/LoginForm/ConfirmationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ConfirmationViewController.swift
+//  LoginForm
+//
+//  Created by User on 18.10.2022.
+//
+
+import UIKit
+
+class ConfirmationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
